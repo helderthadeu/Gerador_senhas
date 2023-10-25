cbed4c13712c07e90cc8a2779b1ac753234c8068
Helder Thadeu <helderthadeubh@gmail.com>
2023-10-25 17:27:16 -0300

    1.0 Versão inicial

--- Gerador_Senhas/src/Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-
-	public static void main(String[] args) {
-		Gerador senha = new Gerador();
-		senha.loop_principal();
-	}
-
-}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Gerador senha = new Gerador();
+		senha.loop_principal();
+	}
+
+}
